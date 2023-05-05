ae7416f765ba79d778700646aca59221ef0bec98
lucas <112901084+LucasSckenal@users.noreply.github.com>
2023-05-05 15:29:35 -0300

    Add files via upload

--- Front-End Design Essencial - HTML, CSS e JS Completo do Zero/PJ FINAL/HTML+CSS+JS/js/main.js
+++ Front-End Design Essencial - HTML, CSS e JS Completo do Zero/PJ FINAL/HTML+CSS+JS/js/main.js
@@ -1,44 +1,70 @@
 //Declarando Variáveis
-var btnContact = document.querySelector(".pj-btn-contact");
-var toggleModal = document.querySelectorAll(".pj-toggle-modal");
+var btnContact = document.querySelector('.pj-btn-contact');
+var toggleModal = document.querySelectorAll('.pj-toggle-modal');
+var toggleMenu = document.querySelectorAll('.pj-toggle-menu');
+var menuMobile = document.querySelector('.pj-menu-mob');
+var btnMenuMobIcon = document.querySelector('.pj-btn-menu-mob ion-icon');
 
 //Page Preloader
-window.addEventListener("load", function () {
-  var pagePreloder = document.querySelector(".pj-preloader");
-  pagePreloder.classList.add("pj-fade-out");
+window.addEventListener('load', function () {
+    var pagePreloder = document.querySelector('.pj-preloader');
+    pagePreloder.classList.add('pj-fade-out');
 
-  setTimeout(function () {
-    pagePreloder.style.display = "none";
-  }, 2000);
+    setTimeout(function () {
+        pagePreloder.style.display = 'none';
+    }, 2000);
 });
 
 //Abrindo e Fechando Informações de Contato
-btnContact.addEventListener("click", function () {
-  var boxContact = document.querySelector(".pj-contact-info");
-  boxContact.classList.toggle("pj-is-open");
-  this.classList.toggle("pj-change-icon");
+btnContact.addEventListener('click', function () {
+    var boxContact = document.querySelector('.pj-contact-info');
+    boxContact.classList.toggle('pj-is-open');
+    this.classList.toggle('pj-change-icon');
 });
 
-//Abrindo e Fechando o Modal de Orcamento
+//Abrindo e Fechando o Menu Mobile
+for (var m = 0; m < toggleMenu.length; m++) {
+    toggleMenu[m].addEventListener('click', function () {
+        var overlay = document.querySelector('.pj-menu-overlay');
+        overlay.classList.toggle('pj-is-open');
+        menuMobile.classList.toggle('pj-menu-is-closed');
+        menuMobile.classList.toggle('pj-menu-is-open');
 
-for (var i = 0; i < toggleModal.length; i++) {
-  toggleModal[i].addEventListener("click", function () {
-    var overlay = document.querySelector(".pj-overlay");
-    var modalOrcamento = document.querySelector("#pj-modal-orcamento");
+        var icon = btnMenuMobIcon.getAttribute('name');
 
-    overlay.classList.toggle("pj-is-open");
-    modalOrcamento.classList.toggle("pj-is-open");
-    modalOrcamento.classList.toggle("pj-slide-top-in");
-  });
+        if (icon === 'menu') {
+            btnMenuMobIcon.setAttribute('name', 'close');
+        } else {
+            btnMenuMobIcon.setAttribute('name', 'menu');
+        }
+
+    });
 }
 
-//Animando Elementos on Scroll com Waypoints
-var myScrollDown = document.querySelector(".pj-scroll-down");
-var waypoint = new Waypoint({
-  element: myScrollDown,
-  handler: function () {
-    myScrollDown.classList.toggle("pj-fade-out");
-  },
-  offset: "80%",
-});
 
+
+//Abrindo e Fechando o Modal de Orcamento
+for (var i = 0; i < toggleModal.length; i++) {
+    toggleModal[i].addEventListener('click', function () {
+        var modalOrcamento = document.querySelector('#pj-modal-orcamento');
+        var overlay = document.querySelector('.pj-overlay');
+        overlay.classList.toggle('pj-is-open');
+        modalOrcamento.classList.toggle('pj-is-open');
+        modalOrcamento.classList.toggle('pj-slide-top-in');
+    });
+}
+
+
+// Animando Elementos da Topbar
+var triggerTopbar = document.querySelector('.pj-trigger-topbar');
+var topbar = document.querySelector('.pj-topbar');
+var logo = document.querySelector('.pj-logo');
+var waypoint = new Waypoint({
+    element: triggerTopbar,
+    handler: function () {
+        topbar.classList.toggle('pj-topbar-bg');
+        logo.classList.toggle('pj-logo-shorten');
+        logo.classList.toggle('pj-logo-big');
+    },
+    offset: '50px'
+});
--- Front-End Design Essencial - HTML, CSS e JS Completo do Zero/PJ FINAL/HTML+CSS+JS/js/slider.js
+++ Front-End Design Essencial - HTML, CSS e JS Completo do Zero/PJ FINAL/HTML+CSS+JS/js/slider.js
@@ -1,173 +1,167 @@
 //PORTFOLIO SLIDER
 
 //Declarando variáveis do slider
-var sliderContainer = document.querySelector(".pj-slider-container");
-var sliderList = document.querySelector(".pj-slider-list");
-var sliderItem = document.querySelectorAll(".pj-slider-item");
+var sliderContainer = document.querySelector('.pj-slider-container');
+var sliderList = document.querySelector('.pj-slider-list');
+var sliderItem = document.querySelectorAll('.pj-portfolio-item');
 const sliderTotalItems = sliderItem.length;
 var sliderListWidth = null;
-var prevItem = document.querySelector(".pj-item-prev");
-var nextItem = document.querySelector(".pj-item-next");
+var prevItem = document.querySelector('.pj-item-prev');
+var nextItem = document.querySelector('.pj-item-next');
 var sliderPos = 0;
-var currentSlide = document.querySelector(".pj-current-slide");
-var totalSlide = document.querySelector(".pj-total-slide");
+var currentSlide = document.querySelector('.pj-current-slide');
+var totalSlide = document.querySelector('.pj-total-slide');
 var currentCounter = 1;
-var navItems = document.querySelectorAll(".pj-item-navigator a");
-var navCounter = document.querySelector(".pj-navigator-counter span");
+var navItems = document.querySelectorAll('.pj-item-navigator a');
+var navCounter = document.querySelector('.pj-navigator-counter span');
+
 
 //Capturando larguras individuais
 var containerWidth = sliderContainer.parentElement.offsetWidth;
 
 //Passando larguras dinâmicas
-sliderContainer.style.width = containerWidth + "px";
+sliderContainer.style.width = containerWidth + 'px';
 
 for (var p = 0; p < sliderItem.length; p++) {
-  sliderItem[p].style.width = containerWidth + "px";
-  var sliderItemWidth = sliderItem[p].offsetWidth;
+    sliderItem[p].style.width = containerWidth + 'px';
+    var sliderItemWidth = sliderItem[p].offsetWidth;
 
-  sliderListWidth += sliderItemWidth;
+    sliderListWidth += sliderItemWidth;
 }
 
-sliderList.style.width = sliderListWidth + "px";
+sliderList.style.width = sliderListWidth + 'px';
+
 
 //Fazendo Animaçao do Slider onClick
+
 
 //HANDLERS
 
 //Next Slide Animaçao
 var nextSlideAnim = function () {
-  var lastItem = sliderListWidth - containerWidth;
+    var lastItem = sliderListWidth - containerWidth;
 
-  if (-1 * sliderPos === lastItem) {
-    return;
-  }
+    if ((-1 * (sliderPos) === lastItem)) {
+        return;
+    }
 
-  sliderPos -= containerWidth;
-  anime({
-    targets: sliderList,
-    translateX: sliderPos,
-    easing: "cubicBezier(0,1.01,.32,1)",
-  });
-};
+    sliderPos -= containerWidth;
+    anime({
+        targets: sliderList,
+        translateX: sliderPos,
+        easing: 'cubicBezier(0,1.01,.32,1)'
+    });
+}
 
 //Prev Slide Animaçao
 var prevSlideAnim = function () {
-  if (sliderPos === 0) {
-    return;
-  }
+    if (sliderPos === 0) {
+        return;
+    }
 
-  sliderPos += containerWidth;
-  anime({
-    targets: sliderList,
-    translateX: sliderPos,
-    easing: "cubicBezier(0,1.01,.32,1)",
-  });
-};
+    sliderPos += containerWidth;
+    anime({
+        targets: sliderList,
+        translateX: sliderPos,
+        easing: 'cubicBezier(0,1.01,.32,1)'
+    });
+}
 
 //Counter Formater
 var counterFormatter = function (n) {
-  if (n < 10) {
-    return "0" + n;
-  } else {
-    return n;
-  }
-};
+    if (n < 10) {
+        return '0' + n;
+    } else {
+        return n;
+    }
+}
 
 //Counter Add
 var counterAdd = function () {
-  if (currentCounter >= 0 && currentCounter < sliderTotalItems) {
-    currentCounter++;
-    currentSlide.innerHTML = counterFormatter(currentCounter);
-    navCounter.innerHTML = counterFormatter(currentCounter);
-  }
-};
+    if ((currentCounter >= 0) && (currentCounter < sliderTotalItems)) {
+        currentCounter++;
+        currentSlide.innerHTML = counterFormatter(currentCounter);
+        navCounter.innerHTML = counterFormatter(currentCounter);
+    }
+}
 
 //Counter Remove
 var counterRemove = function () {
-  if (currentCounter > 1 && currentCounter <= sliderTotalItems) {
-    currentCounter--;
-    currentSlide.innerHTML = counterFormatter(currentCounter);
-    navCounter.innerHTML = counterFormatter(currentCounter);
-  }
-};
+    if ((currentCounter > 1) && (currentCounter <= sliderTotalItems)) {
+        currentCounter--;
+        currentSlide.innerHTML = counterFormatter(currentCounter);
+        navCounter.innerHTML = counterFormatter(currentCounter);
+    }
+}
 
 //Set Active Nav
 var setActiveNav = function () {
-  for (var nv = 0; nv < navItems.length; nv++) {
-    let myNavNum = parseInt(navItems[nv].getAttribute("data-nav"));
+    for (var nv = 0; nv < navItems.length; nv++) {
+        let myNavNum = parseInt(navItems[nv].getAttribute('data-nav'));
 
-    if (myNavNum === currentCounter) {
-      navItems[nv].classList.add("pj-item-active");
+        if (myNavNum === currentCounter) {
+            navItems[nv].classList.add('pj-item-active');
 
-      anime({
-        targets: ".pj-item-active",
-        width: 90,
-      });
+            anime({
+                targets: '.pj-item-active',
+                width: 90
+            });
+        }
     }
-  }
-};
+}
 
 //Set Active Slide
 var setActiveSlide = function () {
-  for (var sld = 0; sld < sliderItem.length; sld++) {
-    let mySlideNum = parseInt(sliderItem[sld].getAttribute("data-slide"));
+    for (var sld = 0; sld < sliderItem.length; sld++) {
+        let mySlideNum = parseInt(sliderItem[sld].getAttribute('data-slide'));
 
-    if (mySlideNum === currentCounter) {
-      sliderItem[sld].classList.add("pj-slide-active");
-      sliderItem[sld]
-        .querySelector(".pj-portfolio-item-box")
-        .classList.add("pj-scale-right");
-      sliderItem[sld]
-        .querySelector(".pj-portfolio-item-thumb img")
-        .classList.add("pj-scale-up");
-      sliderItem[sld]
-        .querySelector(".pj-portfolio-item-info")
-        .classList.add("pj-fade-from-left");
+        if (mySlideNum === currentCounter) {
+            sliderItem[sld].classList.add('pj-slide-active');
+            sliderItem[sld].querySelector('.pj-portfolio-item-box').classList.add('pj-scale-right');
+            sliderItem[sld].querySelector('.pj-portfolio-item-thumb img').classList.add('pj-scale-up');
+            sliderItem[sld].querySelector('.pj-portfolio-item-info').classList.add('pj-fade-from-left');
+        }
     }
-  }
-};
+}
 
 var changeActive = function () {
-  for (var rm = 0; rm < navItems.length; rm++) {
-    navItems[rm].classList.remove("pj-item-active");
-    anime({
-      targets: navItems[rm],
-      width: 20,
-    });
-  }
+    for (var rm = 0; rm < navItems.length; rm++) {
+        navItems[rm].classList.remove('pj-item-active');
+        anime({
+            targets: navItems[rm],
+            width: 20
+        });
+    }
 
-  for (var rms = 0; rms < sliderItem.length; rms++) {
-    sliderItem[rms].classList.remove("pj-slide-active");
-    sliderItem[rms]
-      .querySelector(".pj-portfolio-item-box")
-      .classList.remove("pj-scale-right");
-    sliderItem[rms]
-      .querySelector(".pj-portfolio-item-thumb img")
-      .classList.remove("pj-scale-up");
-    sliderItem[rms]
-      .querySelector(".pj-portfolio-item-info")
-      .classList.remove("pj-fade-from-left");
-  }
-  setActiveNav();
-  setActiveSlide();
-};
+    for (var rms = 0; rms < sliderItem.length; rms++) {
+        sliderItem[rms].classList.remove('pj-slide-active');
+        sliderItem[rms].querySelector('.pj-portfolio-item-box').classList.remove('pj-scale-right');
+        sliderItem[rms].querySelector('.pj-portfolio-item-thumb img').classList.remove('pj-scale-up');
+        sliderItem[rms].querySelector('.pj-portfolio-item-info').classList.remove('pj-fade-from-left');
+    }
+    setActiveNav();
+    setActiveSlide();
+}
+
+
 
 //ACTIONS
 totalSlide.innerHTML = counterFormatter(sliderTotalItems);
 
 anime({
-  targets: ".pj-item-active",
-  width: 90,
+    targets: '.pj-item-active',
+    width: 90
 });
 
-nextItem.addEventListener("click", function () {
-  nextSlideAnim();
-  counterAdd();
-  changeActive();
+
+nextItem.addEventListener('click', function () {
+    nextSlideAnim();
+    counterAdd();
+    changeActive();
 });
 
-prevItem.addEventListener("click", function () {
-  prevSlideAnim();
-  counterRemove();
-  changeActive();
+prevItem.addEventListener('click', function () {
+    prevSlideAnim();
+    counterRemove();
+    changeActive();
 });
